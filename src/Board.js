a89f0b553562bcbcd1b260f47112757c24c32634
北䑓如法 <algebraicallyClosedField@gmail.com>
2021-12-23 10:28:53 +0900

    Change to guruguru game

--- src/Board.js
+++ src/Board.js
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
 import Tile from "./Tile";
 import { TILE_COUNT, GRID_SIZE, BOARD_SIZE } from "./constants"
-import { canSwap, shuffle, swap, isSolved } from "./helpers"
+import { canSwap, shuffle_with_actions, swap, act, isSolved } from "./helpers"
 
-function Board({ imgUrl }) {
+const actions = [
+  {name: "a ↪️️", perm: [4,5,1,0]},
+  {name: "a ↩️", perm: [0,1,5,4]},
+  {name: "b ↪️️️", perm: [9,10,6,5]},
+  {name: "b ↩️", perm: [5,6,10,9]},
+  {name: "c ↪️️️", perm: [6,7,3,2]},
+  {name: "c ↩️", perm: [2,3,7,6]},
+]
+
+function Board() {
+  const imgUrl = 'image2.png';
   const [tiles, setTiles] = useState([...Array(TILE_COUNT).keys()]);
   const [isStarted, setIsStarted] = useState(false);
   console.log('is started:', isStarted)
 
   const shuffleTiles = () => {
-    const shuffledTiles = shuffle(tiles)
+    const shuffledTiles = shuffle_with_actions([...Array(TILE_COUNT).keys()], actions)
     setTiles(shuffledTiles);
   }
 
@@ -20,6 +30,18 @@ function Board({ imgUrl }) {
     }
   }
 
+  const actTiles = (action) => {
+    const actedTiles = act(tiles, action)
+    setTiles(actedTiles)
+  }
+
+  const handleActButtonClick = (actionID) => {
+    const action = actions[actionID]
+    console.log(action)
+    const actedTiles = act(tiles, action)
+    setTiles(actedTiles)
+  }
+  
   const handleTileClick = (index) => {
     swapTiles(index)
   }
@@ -56,7 +78,13 @@ function Board({ imgUrl }) {
           />
         ))}
       </ul>
-      {hasWon && isStarted && <div>Puzzle solved 🧠 🎉</div>}
+      {hasWon && isStarted ? <>🎉🎉解けﾀ━━━(ﾟ∀ﾟ)━━━!!🎉🎉</> : <>.</>}
+      <div className="controller">
+        {actions.map(
+          (a,i) =>
+          <button onClick={() => handleActButtonClick(i)}>{a.name}️</button>
+        )}
+      </div>
       {!isStarted ?
         (<button onClick={() => handleStartClick()}>Start game</button>) :
         (<button onClick={() => handleShuffleClick()}>Restart game</button>)}
