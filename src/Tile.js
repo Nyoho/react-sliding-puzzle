8efd10b1b0c53e7d46e1da36ffdd093368626278
北䑓如法 <algebraicallyClosedField@gmail.com>
2022-12-24 07:06:40 +0900

    Use Framer Motion

--- src/Tile.js
+++ src/Tile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Motion, spring } from "react-motion";
+import { motion } from 'framer-motion'
 import { getMatrixPosition, getVisualPosition } from "./helpers";
 import { TILE_COUNT, GRID_SIZE, BOARD_SIZE } from "./constants"
 
@@ -10,34 +10,23 @@ function Tile(props) {
   const tileStyle = {
     width: `calc(${BOARD_SIZE}px / ${GRID_SIZE})`,
     height: `calc(${BOARD_SIZE}px / ${GRID_SIZE})`,
-    translateX: visualPos.x,
-    translateY: visualPos.y,
     backgroundImage: `url(${imgUrl})`,
     backgroundSize: `${BOARD_SIZE}px`,
     backgroundPosition: `${-(BOARD_SIZE / GRID_SIZE) * (tile % GRID_SIZE)}px ${-(BOARD_SIZE / GRID_SIZE) * (Math.floor(tile / GRID_SIZE))}px`,
     // backgroundPosition: `${(100 / GRID_SIZE) * ((tile % GRID_SIZE))}% ${(100 / TILE_COUNT * GRID_SIZE) * (1 + Math.floor(tile / GRID_SIZE))}%`,
     border: 'dashed 1px #999',
   };
-  const motionStyle = {
-    translateX: spring(visualPos.x),
-    translateY: spring(visualPos.y)
-  }
 
-  return (
-    <Motion style={motionStyle}>
-      {({ translateX, translateY }) => (
-        <li
-          style={{
-            ...tileStyle,
-            transform: `translate3d(${translateX}px, ${translateY}px, 0)`,
-          }}
-          className="tile"
-        >
-          {!imgUrl && `${tile + 1}`}
-        </li>
-      )}
-    </Motion>
-  );
+  return <motion.li
+           animate={{ x:visualPos.x, y: visualPos.y}}
+           transition={{ ease: 'backOut' }}
+           style={{
+             ...tileStyle,
+           }}
+           className="tile"
+         >
+           {!imgUrl && `${tile + 1}`}
+         </motion.li>
 }
 
 export default Tile;
